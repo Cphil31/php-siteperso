4e89273679e1d8c6a5dbca7c31fb0769c051e694
Cphil31 <cecile_philippe@live.fr>
2017-01-06 10:16:15 +0100

    Modifications des chemins dans le menu

--- parts/menu.php
+++ parts/menu.php
@@ -1,6 +1,6 @@
 
 <menu>
-	<ul><a href="index.php?p=menu">menu</a></ul>
+	<ul><a href="index.php">menu</a></ul>
 	<ul><a href="index.php?p=bio">bio</a></ul>
-	<ul><a href="index.php">contact</a></ul>
+	<ul><a href="index.php?p=contact">contact</a></ul>
 </menu>
